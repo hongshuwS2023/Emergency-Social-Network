a4e2a98c2f4738c52f0573f0e3a1df9502251212
Hanqi Dai <hanqid@andrew.cmu.edu>
2023-02-08 02:59:20 -0800

    Finalize iteration 0.

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,33 +1,74 @@
-import { CreateUserInput } from '../../../backend/src/requests/createuser.input';
-import { ErrorMessage } from '../../../backend/src/exceptions/api.exception'
+import { ErrorMessage, parseError } from '../../response/exception.response';
 
-const join = document.getElementById('button');
+let join = document.getElementById('button') || new HTMLDivElement();
+let confirm = document.getElementById('confirm-button') || new HTMLDivElement();
+const modal = document.getElementById("welcomeModal") || new HTMLDivElement();
+const ack = document.getElementById("ackButton") || new HTMLDivElement();
+const buttonClass = document.querySelector('#button-class');
 
 join?.addEventListener('click', async function handleClick(event) {
-    const createUserRequest: CreateUserInput = {
+    const err = document.getElementById('error-message') as HTMLElement || new HTMLDivElement;
+    err.innerHTML = '';
+
+    let createUserRequest = {
         username: (document.getElementById('username') as HTMLInputElement).value || '',
         password: (document.getElementById('password') as HTMLInputElement).value || ''
     }
-
-    await fetch('http://localhost:3001/api/auth/login', {
+    const res = await fetch('http://localhost:3001/api/auth/login', {
         method: 'POST',
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(createUserRequest)
-    }).then(response => response.json()).then(async (res) => {
-        if (res.token) { console.log('login success'); } else if (res.message == '') {
+    }).then(response => { return response.json() })
+    if (res.token) {
+        console.log('login success');
+    }
+    else if (res.message != ErrorMessage.WRONGUSERNAME) {
+        console.log(res);
+        const err = document.getElementById('error-message') as HTMLElement || new HTMLDivElement;
+        err.innerHTML = '';
+        const div = document.createElement("div");
+        div.innerHTML = `<div class="dark:text-white">${parseError(res.message)}</div>`;
+        document.querySelector('#error-message')?.appendChild(div);
+    }
+    else {
+        confirm.classList.remove('invisible');
+        join.classList.add('invisible');
+        buttonClass?.removeChild(join);
+    }
+})
 
-        }
-        else {
-            await fetch('http://localhost:3001/api/auth/register', {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(createUserRequest)
-            }).then(response => response.json()).then(res => { console.log(res) });
-        }
-    });
-    console.log(createUserRequest);
-});
+confirm.addEventListener('click', async function handleClick(event) {
+    const err = document.getElementById('error-message') as HTMLElement || new HTMLDivElement;
+    err.innerHTML = '';
+    let createUserRequest = {
+        username: (document.getElementById('username') as HTMLInputElement).value || '',
+        password: (document.getElementById('password') as HTMLInputElement).value || ''
+    }
+    const res = await fetch('http://localhost:3001/api/auth/register', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(createUserRequest)
+    }).then(response => { return response.json() });
+
+    if (res.token) {
+        console.log('create success');
+        buttonClass?.removeChild(confirm);
+        join.classList.remove('invisible');
+        buttonClass?.appendChild(join);
+        confirm.classList.add('invisible');
+        buttonClass?.appendChild(confirm);
+        modal.style.display = "block"
+        ack.onclick = function () {
+            modal.style.display = "none";
+        };
+    }
+    else {
+        const div = document.createElement("div");
+        div.innerHTML = `<div class="dark:text-white">${parseError(res.message)}</div>`;
+        document.querySelector('#error-message')?.appendChild(div);
+    }
+})
