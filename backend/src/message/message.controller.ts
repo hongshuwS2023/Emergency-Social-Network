fdbaceb05d7e102e5e1eef9230a7a5804427fbe8
Hongshu Wang <123446235+hongshuwS2023@users.noreply.github.com>
2023-02-15 14:24:41 -0800

    socket.io added

--- backend/src/message/message.controller.ts
+++ backend/src/message/message.controller.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Request, Response} from 'express';
 import MessageService from './message.service';
 import { PostMessageInput } from '../requests/postmessage.input';
+import { SocketServer } from '../utils/socketServer';
 
 export default class MessageController {
   messageService: MessageService;
@@ -30,6 +31,7 @@ export default class MessageController {
     try {
       const postMessageInput:PostMessageInput = req.body;
       const message = await this.messageService.postMessage(postMessageInput);
+      SocketServer.io.emit("public message", message);
       res.send(message);
     } catch (error) {
       next(error);
