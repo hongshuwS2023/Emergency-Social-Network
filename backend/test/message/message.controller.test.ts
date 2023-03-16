6e5ab6ad25bfad77978072ebbd644c7f8f14d25d
Hongshu Wang <123446235+hongshuwS2023@users.noreply.github.com>
2023-03-15 21:22:42 -0700

    unit test for message and room finished
    also fix typo in room.controller

--- backend/test/message/message.controller.test.ts
+++ backend/test/message/message.controller.test.ts
@@ -1,99 +1,92 @@
 import AuthController from '../../src/auth/auth.controller';
 import MessageController from '../../src/message/message.controller';
-import { ApiException, ErrorMessage } from '../../src/responses/api.exception';
-import { User } from '../../src/user/user.entity';
+import {ApiException, ErrorMessage} from '../../src/responses/api.exception';
+import { Room } from '../../src/room/room.entity';
+import {User} from '../../src/user/user.entity';
 import ESNDataSource from '../../src/utils/datasource';
 
 const messageController = new MessageController();
-const authController = new AuthController();
+const userRepository = ESNDataSource.getRepository(User);
 
 beforeEach(async () => {
-    await ESNDataSource.initialize();
+  await ESNDataSource.initialize();
+  const user = userRepository.create();
+  user.id = 'test_id';
+  user.username = 'test_username';
+  user.password = 'test_password';
+  user.role = 0;
+  user.status = 0;
+  user.onlineStatus = 0;
+  user.statusTimeStamp = new Date().getTime().toString();
+  user.logoutTime = '';
+  await userRepository.save(user);
+  const roomRepository = ESNDataSource.getRepository(Room);
+  const room = roomRepository.create();
+  room.id = 'public';
+  await roomRepository.save(room);
 });
 
 afterEach(async () => {
-    await ESNDataSource.destroy();
+  await ESNDataSource.destroy();
 });
 
 describe('postMessage', () => {
-    it('Should successfully send the message', async () => {
-        const authUserInput = {
-            username: 'hakan',
-            password: '12345',
-        };
-        await authController.registerUser(authUserInput);
-        const userRepository = ESNDataSource.getRepository(User);
-        const user =
-        (await userRepository.findOneBy({
-            username: authUserInput.username,
-        })) || new User();
+  it('Should successfully send the message', async () => {
+    const postMessageInput = {
+      userId: 'test_id',
+      content: 'hello',
+      roomId: 'public',
+    };
+    expect(
+      await messageController.postMessage(postMessageInput)
+    ).not.toBeNull();
+  });
 
-        const postMessageInput = {
-            userId: user.id,
-            content: 'hello',
-            roomId: 'public',
-        };
-        
-        expect(await messageController.postMessage(postMessageInput)).not.toBeNull();
+  it('Should not send messages if input is invalid', async () => {
 
-    });
+    // Case user id is invalid
+    const invalidUserIdMessageInput = {
+      userId: 'invalid id',
+      content: 'hello',
+      roomId: 'public',
+    };
 
-    it('Should not send messages if input is invalid', async () => {
-        const authUserInput = {
-            username: 'hakan',
-            password: '12345',
-        };
-        const userRepository = ESNDataSource.getRepository(User);
-        const user =
-        (await userRepository.findOneBy({
-            username: authUserInput.username,
-        })) || new User();
+    try {
+      await messageController.postMessage(invalidUserIdMessageInput);
+    } catch (error) {
+      expect((<ApiException>error).error_message).toBe(
+        ErrorMessage.WRONGUSERNAME
+      );
+    }
 
-        // Case user id is invalid
-        await authController.registerUser(authUserInput);
+    // Case message is empty
+    const emptyMessageInput = {
+      userId: 'test_id',
+      content: '',
+      roomId: 'public',
+    };
 
-        const invalidUserIdMessageInput = {
-            userId: 'invalid id',
-            content: 'hello',
-            roomId: 'public',
-        };
-        
-        try {
-            await messageController.postMessage(invalidUserIdMessageInput);
-          } catch (error) {
-            expect((<ApiException>error).error_message).toBe(
-              ErrorMessage.WRONGUSERNAME
-            );
-          }
+    try {
+      await messageController.postMessage(emptyMessageInput);
+    } catch (error) {
+      expect((<ApiException>error).error_message).toBe(
+        ErrorMessage.EMPTYMESSAGE
+      );
+    }
 
-        // Case message is empty
-        const emptyMessageInput = {
-            userId: user.id,
-            content: '',
-            roomId: 'public',
-        };
-        
-        try {
-            await messageController.postMessage(emptyMessageInput);
-          } catch (error) {
-            expect((<ApiException>error).error_message).toBe(
-              ErrorMessage.EMPTYMESSAGE
-            );
-          }
+    // Case room is not valid
+    const invalidRoomMessageInput = {
+      userId: 'test_id',
+      content: 'hello',
+      roomId: 'invalid room',
+    };
 
-         // Case room is not valid
-         const invalidRoomMessageInput = {
-             userId: user.id,
-             content: 'hello',
-             roomId: 'invalid room',
-         };
-         
-         try {
-             await messageController.postMessage(invalidRoomMessageInput);
-           } catch (error) {
-             expect((<ApiException>error).error_message).toBe(
-               ErrorMessage.ROOMIDNOTFOUND
-             );
-           }
-    });
+    try {
+      await messageController.postMessage(invalidRoomMessageInput);
+    } catch (error) {
+      expect((<ApiException>error).error_message).toBe(
+        ErrorMessage.ROOMIDNOTFOUND
+      );
+    }
+  });
 });
